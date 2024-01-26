aee965c6b48dd688f5ffbea198eb9dd7a7509ea4
Diego VillatoroMx <107954399+diegovillatoromx@users.noreply.github.com>
2024-01-26 07:29:20 -0600

    Update generate_purchase_data.py

--- ECommerce-Sales-Analysis-and-Streaming-Monitoring/data_generation/generate_purchase_data.py
+++ ECommerce-Sales-Analysis-and-Streaming-Monitoring/data_generation/generate_purchase_data.py
@@ -8,7 +8,7 @@ KinesisClient = boto3.client('kinesis', region_name=region)
 
 # Generate purchase data
 purchase_data = generate_purchase_data(1000)  # Generate 100 purchase records
-
+ 
 for purchase in purchase_data:
     record_value = json.dumps(purchase).encode('utf-8')
     records = KinesisClient.put_record(StreamName=stream_name, Data=record_value, PartitionKey='USER_ID')
